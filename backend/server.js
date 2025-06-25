f09dcd086a585dac5cf36bcece78b57b9e388efa
Nikhilesh <nikhileshgowda10@gmail.com>
2025-06-25 11:46:07 -0700

    commit 1

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3001;
+const port = 3510;
 
 // PostgreSQL database connection configuration
 const pool = new Pool({
@@ -164,5 +164,5 @@ app.delete('/api/claims', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://44.223.13.180:${port}`);
+    console.log(`Server running at http://44.201.134.165:${port}`);
 });
